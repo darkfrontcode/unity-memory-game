38c9935fb659b0e06007ad7ef6509f9ce8e327ad
Ueslei Stevans F. De Souza <ueslei.souza@cwi.com.br>
2018-10-23 12:48:21 -0300

    nobody cares

--- memory-game/Assets/Scripts/GameController.cs
+++ memory-game/Assets/Scripts/GameController.cs
@@ -6,10 +6,55 @@ using System.Collections.Generic;
 public class GameController : MonoBehaviour
 {
     public List<Button> btns = new List<Button>();
+    public Sprite[] puzzles;
+    public List<Sprite> gamePuzzles = new List<Sprite>();
 
-	public void Start ()
+    private bool firstGuess, secondGuess;
+
+    private int countGuesses;
+    private int countCorrectGuesses;
+    private int gameGuesses;
+
+    private int firstGuessIndex, secondGuessIndex;
+
+    private string firstGuessPuzzle, secondGuessPuzzle;
+
+    [SerializeField]
+    private Sprite bgImage;
+
+    public void Awake()
+    {
+        puzzles = Resources.LoadAll<Sprite>("Sprites");
+    }
+
+    public void Start ()
 	{
         GetButtons();
+        AddListener();
+        AddGamePuzzles();
+    }
+
+    public void PickAPuzzle()
+    {
+        string name = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+        Debug.Log($"You Are Cliking a button named {name}");
+    }
+
+    public void AddGamePuzzles()
+    {
+        int looper = btns.Count;
+        int index = 0;
+
+        for(int i = 0; i < looper; i++)
+        {
+            if(index == looper / 2)
+            {
+                index = 0;
+            }
+
+            gamePuzzles.Add(puzzles[index]);
+            index++;
+        }
     }
 
     private void GetButtons()
@@ -19,6 +64,15 @@ public class GameController : MonoBehaviour
         for (int i = 0; i < objects.Length; i++)
         {
             btns.Add(objects[i].GetComponent<Button>());
+            btns[i].image.sprite = bgImage;
+        }
+    }
+
+    private void AddListener()
+    {
+        foreach (Button btn in btns)
+        {
+            btn.onClick.AddListener(PickAPuzzle);
         }
     }
 }
